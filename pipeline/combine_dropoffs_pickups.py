8343e4965a174b18fba4a2fb1eae6f8790372cfa
daihuanlin <daihuanlin@uchicago.edu>
2024-02-04 16:47:01 -0600

    transfered combine_dropoffs_pickups.ipynb code to script

--- pipeline/combine_dropoffs_pickups.py
+++ pipeline/combine_dropoffs_pickups.py
@@ -8,15 +8,18 @@ if __name__ == 'main':
     config = configparser.ConfigParser()
     config.read("../pipeline/utils/config_inputs.ini")
     cfg = config["combine.dropoffs_pickups"]
-    
+
+    # parse config
     df_path = cfg["toy_example_df_path"]
     dist_path = cfg["toy_example_dist_path"]
     output_res_df_path = cfg["output_res_df_path"]
     output_res_dist_path = cfg["output_res_dist_path"]
-    
+
+    # read input dataframes
     df = pd.read_csv(df_path)
     dists_df = pd.read_csv(dist_path)
 
+    # set up for 
     dists_df.columns = [i for i in range(len(dists_df.columns))]
 
     res_df, res_dists = df, dists_df
@@ -28,7 +31,7 @@ if __name__ == 'main':
             res_df = pd.concat([res_df, row_copy])
     
             dist_copy = pd.DataFrame(res_dists.iloc[i]).T
-            res_dists = pd.concat([res_dists, dist_copy])
+            res_dists = pd.concat([res_dists, dist_copy]) 
             if res_dists.isnull().values.any():
                 print(f"NaN detected while duplicating rows in res_dists on iteration {i}")
                 print(res_dists)
