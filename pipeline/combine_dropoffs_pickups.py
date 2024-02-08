b3d9397055cb6836861e69c46e44e80460137da5
daihuanlin <daihuanlin@uchicago.edu>
2024-02-08 09:28:16 -0600

    moving back to duplicating rows

--- pipeline/combine_dropoffs_pickups.py
+++ pipeline/combine_dropoffs_pickups.py
@@ -20,8 +20,23 @@ if __name__ == '__main__':
     df = pd.read_csv(pickups_df_path)
     dists_df = pd.read_csv(pickups_dist_path)
 
+    # create new "Capacity" column = "Pickups" - "Dropoffs"
     res_df, res_dists = df, dists_df
-    res_df['Capacity'] = res_df['Daily_Pickup_Totes'] - res_df['Weekly_Dropoff_Totes']
+    res_dists.columns = [i for i in range(len(res_dists.columns))]
+
+    capacities = list(df['Daily_Pickup_Totes'])
+    for i, row in df.iterrows():
+        if row['Weekly_Dropoff_Totes'] != 0:
+            capacities.append(-1 * row['Weekly_Dropoff_Totes'])
+            row_copy = pd.DataFrame(row).T
+            res_df = pd.concat([res_df, row_copy])
+
+            dist_copy = pd.DataFrame(res_dists.iloc[i]).T
+            res_dists = pd.concat([res_dists, dist_copy])
+
+            res_dists[len(res_dists.columns)] = res_dists[i]
+    res_df['Capacity'] = capacities
+    res_dists = res_dists.reset_index(drop = True)
 
     # save outputs
     res_df.to_csv(combined_df_path, index = False)
