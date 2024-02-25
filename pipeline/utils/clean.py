f26d0597275172562c2274add19aa77373c92455
jescib <jescib123@gmail.com>
2024-02-24 19:05:00 -0600

    changes to clean.py to correctly save csv file

--- pipeline/utils/clean.py
+++ pipeline/utils/clean.py
@@ -1,14 +1,48 @@
-#file used for cleaning scraped data
 import pandas as pd
+import json
 
+# Load JSON data from file
+with open('data/poi/hilo_yelp_original.json', 'r') as file:
+    hilo_data_list = json.load(file)
 
-filepath = 'data/poi/hilo_yelp_original.json'
+# Initialize lists to store extracted fields
+ids = []
+names = []
+categories_list = []
+longitudes = []
+latitudes = []
+display_addresses_list = []
 
-yelp_scrape_dataframe = pd.read_json(filepath)
+# Iterate over each JSON object in the list
+for hilo_data in hilo_data_list:
+    # Extract fields from the current JSON object
+    id = hilo_data['id']
+    name = hilo_data['name']
+    categories = ', '.join([category['title'] for category in hilo_data['categories']])
+    longitude = hilo_data['coordinates']['longitude']
+    latitude = hilo_data['coordinates']['latitude']
+    display_address = ', '.join(hilo_data['location']['display_address'])
+    
+    # Append extracted fields to the respective lists
+    ids.append(id)
+    names.append(name)
+    categories_list.append(categories)
+    longitudes.append(longitude)
+    latitudes.append(latitude)
+    display_addresses_list.append(display_address)
 
+# Create DataFrame
+hilo_yelp_locations = pd.DataFrame({
+    'id': ids,
+    'name': names,
+    'categories': categories_list,
+    'longitude': longitudes,
+    'latitude': latitudes,
+    'display_address': display_addresses_list
+})
 
-
-csv_filepath = 'data/poi/yelp_data.csv'
+# Assuming you have already created the DataFrame 'hilo_yelp_locations'
 
 # Save DataFrame as CSV
-yelp_scrape_dataframe.to_csv(csv_filepath, index=False)
+hilo_yelp_locations.to_csv('data/poi/hilo_yelp_locations.csv', index=False)
+
